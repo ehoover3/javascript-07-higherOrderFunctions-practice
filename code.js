50a808e2ecaff039b06f1e14554a85e274a8a64a
Eric Hoover <ehoover3@yahoo.com>
2022-02-01 20:31:05 -0800

    updated instructions

--- code.js
+++ code.js
@@ -4,17 +4,17 @@ const originalArray = [5, 933, 26, 33, 20, 10, 555, 884, 2, 10];
 
 // 1. Use "Array.map" to Show the Original Array Again
 let answer1div = document.querySelector("#answer1");
-let answer1 = originalArray.map((item) => item);
+let answer1 = null; // replace null with your code
 answer1div.textContent = answer1;
 
 // 2. Use "Array.map" to Show the Original Array Multiplied by 2
 let answer2div = document.querySelector("#answer2");
-let answer2 = originalArray.map((batman) => batman * 2);
+let answer2 = null; // replace null with your code
 answer2div.textContent = answer2;
 
 // 3. Use "Array.filter" to Show Only the Number 26
 let answer3div = document.querySelector("#answer3");
-let answer3 = originalArray.filter((index) => index === 26);
+let answer3 = null; // replace null with your code
 answer3div.textContent = answer3;
 
 // 4. Use "Array.filter" to Show Even Numbers
@@ -28,19 +28,19 @@ answer3div.textContent = answer3;
 // ... so any number % 2 that equals 0 is even
 // ... and any number % 2 that equals 1 is odd
 let answer4div = document.querySelector("#answer4");
-let answer4 = originalArray.filter((index) => index % 2 === 0);
+let answer4 = null; // replace null with your code
 answer4div.textContent = answer4;
 
 // 5. Use "Array.filter" to Show Odd Numbers
 let answer5div = document.querySelector("#answer5");
-let answer5 = originalArray.filter((index) => index % 2 === 1);
+let answer5 = null; // replace null with your code
 answer5div.textContent = answer5;
 
-// 6. 6. You are given an array of objects representing data about developers who are attending a meetup.
+// 6. You are given an array of objects data about developers.
 // Your task is to return the number of developers coming from Europe.
 
 // For example, given the following list:
-let guestList = [
+let people = [
   {
     firstName: "Noah",
     country: "Switzerland",
@@ -68,10 +68,6 @@ let guestList = [
   },
 ];
 
-// write your code here
-// Solution A
-
 let answer6div = document.querySelector("#answer6");
-let europeanGuests = guestList.filter((guest) => guest.continent === "Europe");
-let answer6 = europeanGuests.length;
+let answer6 = null; // replace null with your code
 answer6div.textContent = answer6;
